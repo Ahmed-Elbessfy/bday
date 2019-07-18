88ad54b43bac68c5234bf4501eab1b06f7bdc496
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-07-18 21:35:24 +0200

    add project imgs and complete Context
    - add imgs and import them in Context
    - assign each img to the right  place
    - incresae gifts to be 6
    - edit likes users data to be a name and pic in an Objects
    -  add axios request main code
    - beautfiy the code

--- src/Context.js
+++ src/Context.js
@@ -1,40 +1,107 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import profileUserPic from './media/profile.png'
+import React, { Component } from "react";
+import axios from "axios";
+import profileUserPic from "./media/profile.png";
+import bmw from "./media/bmw.jpg";
+import classico from "./media/classico.jpg";
+import fbshoes from "./media/fbshoes.jpg";
+import iphone from "./media/iphone.png";
+import maldives from "./media/maldives.jpg";
+import playstation from "./media/playstation.jpg";
+import likeUser1 from "./media/likeUsers (1).jpg";
+import likeUser2 from "./media/likeUsers (2).jpg";
+import likeUser3 from "./media/likeUsers (3).jpg";
+import likeUser4 from "./media/likeUsers (4).png";
+import likeUser5 from "./media/likeUsers (5).png";
+import likeUser6 from "./media/likeUsers (6).jpg";
 
 const Context = React.createContext();
 
 class Provider extends Component {
   state = {
     profileUser: {
-      name: 'ahmed taha',
+      name: "ahmed taha",
       profilePic: `${profileUserPic}`
     },
-    wishList: [{
-      id: 1,
-      name: 'gift 1',
-      img: '',
-      likes: ['user', 'user', 'user', 'user', 'user']
-    }, {
-      id: 2,
-      name: 'gift 2',
-      img: '',
-      likes: ['user', 'user', 'user', 'user', 'user']
-    }, {
-      id: 3,
-      name: 'gift 3',
-      img: '',
-      likes: ['user', 'user', 'user', 'user', 'user']
-    },]
+    wishList: [
+      {
+        id: 1,
+        name: "BMW ",
+        img: `${bmw}`,
+        likes: [
+          { name: "mark", pic: `${likeUser1}` },
+          { name: "emma ", pic: `${likeUser2}` },
+          { name: "micky", pic: `${likeUser3}` },
+          { name: "leonardo", pic: `${likeUser4}` }
+        ]
+      },
+      {
+        id: 2,
+        name: "classico tickets",
+        img: `${classico}`,
+        likes: [
+          { name: "leonardo", pic: `${likeUser4}` },
+          { name: "micky", pic: `${likeUser3}` },
+          { name: "emma ", pic: `${likeUser2}` }
+        ]
+      },
+      {
+        id: 3,
+        name: "maldives vacation",
+        img: `${maldives}`,
+        likes: [
+          { name: "jhon", pic: `${likeUser5}` },
+          { name: "emy", pic: `${likeUser6}` },
+          { name: "mark", pic: `${likeUser1}` },
+          { name: "emma ", pic: `${likeUser2}` }
+        ]
+      },
+      {
+        id: 4,
+        name: "iphone max",
+        img: `${iphone}`,
+        likes: [
+          { name: "micky", pic: `${likeUser3}` },
+          { name: "leonardo", pic: `${likeUser4}` },
+          { name: "jhon", pic: `${likeUser5}` },
+          { name: "emy", pic: `${likeUser6}` }
+        ]
+      },
+      {
+        id: 5,
+        name: "playstation 4",
+        img: `${playstation}`,
+        likes: [
+          { name: "emma ", pic: `${likeUser2}` },
+          { name: "micky", pic: `${likeUser3}` },
+          { name: "leonardo", pic: `${likeUser4}` }
+        ]
+      },
+      {
+        id: 6,
+        name: "football shoes",
+        img: `${fbshoes}`,
+        likes: [
+          { name: "emy", pic: `${likeUser6}` },
+          { name: "emma ", pic: `${likeUser2}` }
+        ]
+      }
+    ]
+  };
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/todos/1`).then(res => {
+      console.log(res);
+      // this.setState({res:state})
+    });
   }
+
   render() {
     return (
-      <Context.Provider value={ this.state } >
-        { this.props.children }
+      <Context.Provider value={this.state}>
+        {this.props.children}
       </Context.Provider>
     );
   }
 }
-const Consumer = Context.Consumer
-export { Provider, Consumer }
-
+const Consumer = Context.Consumer;
+export { Provider, Consumer };
--- src/components/GiftPage.js
+++ src/components/GiftPage.js
@@ -1,12 +1,9 @@
-import React from 'react'
-import { Consumer } from '../Context'
+import React from "react";
+import { Consumer } from "../Context";
 import { Link } from "react-router-dom";
 
-
 function GiftPage(props) {
-  return (
-    <div> Gift Page</div>
-  )
+  return <div> Gift Page</div>;
 }
 
-export default GiftPage
+export default GiftPage;
