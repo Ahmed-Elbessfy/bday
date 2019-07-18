bf740e67c2c4616a9a244cc6a7299ecdf70d9f84
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-07-18 17:40:32 +0200

    add Routes in App comp
    - add Routes to Profile & GiftPage comp.s

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Profile from './components/Profile'
+import GiftPage from './components/GiftPage'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from './Context'
+
 
 function App() {
   return (
@@ -11,7 +13,10 @@ function App() {
       <Router>
         <div className="App">
           <Nav />
-          <Profile />
+          <Switch>
+            <Route exact path='/' component={ Profile } />
+            <Route path='/gift/:id' component={ GiftPage } />
+          </Switch>
         </div>
       </Router>
     </Provider>
