c9aa09beffd59fb7ddf4597aba7e41b588261b18
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-07-18 22:43:06 +0200

    style and beautify Nav comp
    - add styling classes to Nav comp
    - add extra small style rules for Nav in App.css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,21 +3,53 @@ import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar fixed-top">
+    <nav
+      className="navbar fixed-top bg-white"
+      style={{ minHeight: "10vh", borderBottom: "2px solid #dcdcdc" }}
+    >
       <div className="container">
-        <div className="brand">bday</div>
-        <ul>
-          <li>
-            <NavLink to="/users">users</NavLink>
+        <div className="brand text-capitalize h1 text-danger">bday</div>
+        <ul className="list-unstyled mb-0" style={{ display: "flex" }}>
+          <li className="mr-3">
+            <NavLink
+              exact
+              to="/"
+              activeClassName="text-danger"
+              className="text-capitalize text-secondary"
+              style={{ fontSize: "1.2rem" }}
+            >
+              users
+            </NavLink>
           </li>
-          <li>
-            <NavLink to="#">gifts</NavLink>
+          <li className="mr-3">
+            <NavLink
+              to="#"
+              activeClassName="text-danger"
+              className="text-capitalize text-secondary"
+              style={{ fontSize: "1.2rem" }}
+            >
+              gifts
+            </NavLink>
           </li>
-          <li>
-            <NavLink to="#">notifications</NavLink>
+          <li className="mr-3">
+            <NavLink
+              to="#"
+              activeClassName="text-danger"
+              className="text-capitalize text-secondary"
+              style={{ fontSize: "1.2rem" }}
+            >
+              notifications
+            </NavLink>
           </li>
-          <li>
-            <NavLink to="#">profile</NavLink>
+          <li className="mr-3">
+            <NavLink
+              to="#"
+              activeClassName="text-danger"
+              className="text-capitalize text-secondary"
+              style={{ fontSize: "1.2rem" }}
+            >
+              profile
+            </NavLink>
           </li>
         </ul>
       </div>
