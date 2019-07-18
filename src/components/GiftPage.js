d7e68fef86fda0c31e4aa61a2692168a8b0ae61d
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-07-18 22:08:18 +0200

    complete GiftPage comp
    - add GiftPage content
    - style GiftPage content

--- src/components/GiftPage.js
+++ src/components/GiftPage.js
@@ -3,7 +3,122 @@ import { Consumer } from "../Context";
 import { Link } from "react-router-dom";
 
 function GiftPage(props) {
-  return <div> Gift Page</div>;
+  return (
+    <Consumer>
+      {value => {
+        const { wishList: gifts, profileUser: user } = value;
+        let gift = gifts.filter(gift => {
+          return gift.id === Number(props.match.params.id);
+        })[0];
+        return (
+          <div className="container">
+            <Link
+              to="/"
+              className="btn btn-danger btn-lg rounded-pill px-5 ml-3 text-capitalize my-5"
+            >
+              go back
+            </Link>
+            <div className="user">
+              <img
+                src={user.profilePic}
+                style={{ width: "6rem" }}
+                className="rounded-circle m-4"
+                alt={user.name}
+              />
+              <span className="text-secondary text-capitalize h4 ">
+                {user.name}
+              </span>
+            </div>
+            <div className="row">
+              <div className="col-md-6 text-center shadow p-0 my-4">
+                <img src={gift.img} alt={gift.name} className="w-100 h-100" />
+              </div>
+              <div className="col-md-6 pl-5 py-2">
+                <p className="h1 text-secondary my-2 text-capitalize text-sm-center">
+                  {gift.name}
+                </p>
+                <div className="row my-5">
+                  <div className="col-lg-3 col-sm-6 p-0">
+                    <button
+                      className="btn text-secondary text-capitalize w-100 border p-3"
+                      style={{ fontSize: "1rem" }}
+                    >
+                      <i
+                        className="fas fa-heart fa-2x w-100 mb-2"
+                        aria-hidden="true"
+                      />
+                      like
+                    </button>
+                  </div>
+
+                  <div className="col-lg-3 col-sm-6 p-0">
+                    <button
+                      className="btn text-secondary text-capitalize w-100 border p-3"
+                      style={{ fontSize: "1rem" }}
+                    >
+                      <i
+                        className="fas fa-star fa-2x  w-100 mb-2"
+                        aria-hidden="true"
+                      />
+                      wish itl
+                    </button>
+                  </div>
+
+                  <div className="col-lg-3 col-sm-6 p-0">
+                    <button
+                      className="btn text-secondary text-capitalize w-100 border p-3"
+                      style={{ fontSize: "1rem" }}
+                    >
+                      <i
+                        className="fas fa-lock fa-2x  w-100 mb-2"
+                        aria-hidden="true"
+                      />
+                      reserve
+                    </button>
+                  </div>
+
+                  <div className="col-lg-3 col-sm-6 p-0">
+                    <button
+                      className="btn text-secondary text-capitalize w-100 border p-3"
+                      style={{ fontSize: "1rem" }}
+                    >
+                      <i
+                        className="fas fa-comment-alt fa-2x  w-100 mb-2"
+                        aria-hidden="true"
+                      />
+                      gift group
+                    </button>
+                  </div>
+                </div>
+                <p className="h3 text-secondary">
+                  Liked by {gift.likes.length} people
+                </p>
+                <ul className="list-unstyled text-secondary">
+                  {gift.likes.map(user => {
+                    return (
+                      <li
+                        key={user.name}
+                        style={{ listStyle: "none", fontSize: "1.4rem" }}
+                        className="p-2 text-capitalize"
+                      >
+                        <img
+                          src={user.pic}
+                          alt={user.name}
+                          className="rounded-circle"
+                          style={{ width: "3rem" }}
+                        />
+                        <span className="ml-3">{user.name}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+        );
+      }}
+    </Consumer>
+  );
 }
 
 export default GiftPage;
