184422e613500e2da93e1c8ddc3221d0507ae31a
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-07-20 04:05:03 +0200

    Updates

---
+++ precache-manifest.d730e8d5eff8a11e10b544184a595da9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b28fec17a9396efb7e25aa4924a627",
+    "url": "/bday/index.html"
+  },
+  {
+    "revision": "5c9fe39433db64ebabf3",
+    "url": "/bday/static/css/main.00930005.chunk.css"
+  },
+  {
+    "revision": "34b900429674a8d4781f",
+    "url": "/bday/static/js/2.7ce673fb.chunk.js"
+  },
+  {
+    "revision": "5c9fe39433db64ebabf3",
+    "url": "/bday/static/js/main.8a1a0149.chunk.js"
+  },
+  {
+    "revision": "ab61faf67ecf960768e9",
+    "url": "/bday/static/js/runtime~main.99a1866c.js"
+  },
+  {
+    "revision": "a45ede9b30b5565215914aa6631c4e6d",
+    "url": "/bday/static/media/bmw.a45ede9b.jpg"
+  },
+  {
+    "revision": "3f035725108d63816bd08c4ad479c500",
+    "url": "/bday/static/media/classico.3f035725.jpg"
+  },
+  {
+    "revision": "d19c89714acaf06b49b32f5c5c0a9bca",
+    "url": "/bday/static/media/cover.d19c8971.jpeg"
+  },
+  {
+    "revision": "22878c5f73161a2447766cf110a7cb44",
+    "url": "/bday/static/media/fbshoes.22878c5f.jpg"
+  },
+  {
+    "revision": "08ab5e8d580bb78e3cc10dd6a0f88bbb",
+    "url": "/bday/static/media/iphone.08ab5e8d.png"
+  },
+  {
+    "revision": "3cd91562fd6cd2dcc3001480de180ef4",
+    "url": "/bday/static/media/likeUsers (2).3cd91562.jpg"
+  },
+  {
+    "revision": "11ad8c0bce945688272b741e76846afc",
+    "url": "/bday/static/media/likeUsers (3).11ad8c0b.jpg"
+  },
+  {
+    "revision": "c809cae9e5677ef0e4ac875e47c5b956",
+    "url": "/bday/static/media/maldives.c809cae9.jpg"
+  },
+  {
+    "revision": "10662a758a87c874a7c407fbbe1ea544",
+    "url": "/bday/static/media/playstation.10662a75.jpg"
+  },
+  {
+    "revision": "c9c72ab3815d658b65daabfea6ea10ba",
+    "url": "/bday/static/media/profile.c9c72ab3.png"
+  }
+]);
